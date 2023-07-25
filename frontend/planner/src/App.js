061072a67b416b356c332ec210e092b8286cfc9a
MizaelJR17 <mizael.jxjunior@aluno.educacao.pe.gov.br>
2023-07-24 21:39:00 -0300

    Atividade Map - Frontend

--- frontend/planner/src/App.js
+++ frontend/planner/src/App.js
@@ -2,12 +2,34 @@ import './App.css';
 import React from 'react';
 
 function App() {
+  
+  const atividades = [
+    {
+      id: 1,
+      descricao:'Primeira atividade'
+    },
+    {
+      id: 1,
+      descricao:'Segunda atividade'
+    }
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
+   <>
+    <form>
+      <input id="id" type ="text" placeholder="id"/>
+      <input id="descricao" type ="text" placeholder="descricao"/>
+      <button> + Atividade</button>
+    </form>
+   <div className='mt-3'>
+      <ul className= "list-group">
+        {atividades.map(ativ => (<li key ={ativ.id} className='list-group-item'>{ativ.id} - {ativ.descricao}</li>
+        ))}
+      </ul>
     </div>
+  
+  </>
   );
 }
 
-export default App;
+export default App; 
