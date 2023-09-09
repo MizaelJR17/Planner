c0e707abc99f2df52e09bf38b5e55a01163237da
MizaelJR17 <mizael.jxjunior@aluno.educacao.pe.gov.br>
2023-09-09 12:03:15 -0300

    Ajustando Form

--- frontend/planner/src/App.js
+++ frontend/planner/src/App.js
@@ -16,13 +16,62 @@ let initialState =[
 
 function App() {
   const [atividades, setAtividades] = useState(initialState)
-   
+
+  function addAtvidades(e){
+    e.preventDefault();
+
+    const atividade = {
+      id: document.getElementById('id').value,
+      descricao: document.getElementById('descricao').value ,
+
+
+    };
+    setAtividades([...atividades,{...atividade}]);
+  }
+  
+
+
+
   return (
    <>
-    <form>
-      <input id="id" type ="text" placeholder="id"/>
-      <input id="descricao" type ="text" placeholder="descricao" />
-      <button> + Atividade</button>
+    <form className='row g-3'>
+      <div className='col-md-6'>
+        <label className='form-label'>
+          Id
+        </label>
+        <input id="id" type ="text" className='form-control'/>
+      </div>
+        <div className='col-md-6'>
+          <label className='form-label'>Prioridade</label>
+          <select id="inputState" className="form-control">
+            <option defaltValue="0">Selecione ...</option>
+            <option defaltValue="1">Baixa</option>
+            <option defaltValue="2">Normal</option>
+            <option defaltValue="3">Alta</option>
+          </select>
+        </div>
+
+      <div className='col-md-6'>
+        <label className='form-label'>
+          Título
+        </label>
+      <input id="titulo" type ="text" className='form-control'/>
+      </div>
+      
+      
+      <div className='col-md-6'>
+        <label className='form-label'>
+          Descrição
+        </label>
+      <input id="descricao" type ="text" className='form-control'/>
+      </div>
+      
+      <div class='col-12'>
+      <button className='btn btn-outline-secondary'> 
+        + Atividade
+      </button>
+      </div>
+     
     </form>
    <div className='mt-3'>
       <ul className= "list-group">
